1543b273f954c36d90e382bb0a356a9182501d0b
caojiaquan <2811251638@qq.com>
2017-09-21 19:43:17 +0800

    f_upd

---
+++ dialog/tpl.js
@@ -0,0 +1,45 @@
+<img class="dialogFlag" src="http://img003.qufenqi.com/products/c7/1f/c71fdf369cbe48617dc7af8ba721109f.gif"></img>
+        <div class="dialog">
+        <div class="dialog-header">
+        <span class="dialog-service">客服中心</span>
+        <span class="dialog-date">更新于：2017.09.09</span>
+    <span class="dialog-log">日志</span>
+        </div>
+        <div class="dialog-dialogue-box">
+        <div class="dialog-dialogue left">
+        <span class="iconfont icon-kefu icon"></span>
+        <span class="triangle left"></span>
+        <span class="dialog-message left">一组用于配置Ajax请求的键/值对。除了url以外的所有选项属性都是可选的。任何默认选项可以用$.ajaxSetup()进行设置。。</span>
+    </div>
+    <div class="dialog-dialogue right">
+        <span class="iconfont icon-yonghu icon"></span>
+        <span class="triangle right"></span>
+        <span class="dialog-message right">。任何默认选项可以用$.ajaxSetup()进行设置。所有选项设置的完整列表，请参阅jQuery.ajax( settings )。 type（类型）选项将自动设置为POST。</span>
+    </div>
+    <div class="dialog-dialogue left">
+        <span class="iconfont icon-kefu icon"></span>
+        <span class="triangle left"></span>
+        <span class="dialog-message left">一组用于配置Ajax请求的键/值对。除了url以外的所有选项属性都是可选的。任何默认选项可以用$.ajaxSetup()进行设置。。</span>
+    </div>
+    <div class="dialog-dialogue left">
+        <ol>
+        <h3>相关搜索</h3>
+        <li><a href="">一组用于配置Ajax请求的键/值对</a></li>
+    <li><a href="">一组用于配置Ajax请求的键/值对</a></li>
+    <li><a href="">一组用于配置Ajax请求的键/值对</a></li>
+    <li><a href="">一组用于配置Ajax请求的键/值对</a></li>
+    <li><a href="">一组用于配置Ajax请求的键/值对</a></li>
+    </ol>
+    <!--<span class="iconfont icon-kefu icon"></span>-->
+        <!--<span class="triangle left"></span>-->
+        <!--<span class="dialog-message left">一组用于配置Ajax请求的键/值对。除了url以外的所有选项属性都是可选的。任何默认选项可以用$.ajaxSetup()进行设置。。</span>-->
+    </div>
+    </div>
+    <div class="dialog-footer">
+        <textarea type="text" name="message" value="" class="dialogMes"></textarea>
+        <div class="dialog-submit">
+        <span>按下回车键发送</span>
+        <button>发送</button>
+        </div>
+        </div>
+        </div>
